6d63adee9637087fced0621850efbb0be39a0976
NevinBonak <45405843+NevinBonak@users.noreply.github.com>
2020-09-27 10:29:20 -0400

    parse.py is written but doesn't work perfectly yet, also updated requirements for query.py

--- query.py
+++ query.py
@@ -1,8 +1,9 @@
 def query(index, column, table):
     # query takes 3 arguments
-    # index is whatever the user puts between quotes, column is whatever is after the quotes, which is the column of one of the tables
+    # index is whatever the user puts between quotes or the name of the team, column should correspond with the column of a table.
     # table is which table needs to be queried
     # if whatever the user requested does not exist query should return -1
+    # if the database has not been loaded yet query should return -2
 
     #format query from arg
 
--- query_test.py
+++ query_test.py
@@ -1,3 +1,4 @@
 #used to test parser.py without modifying query.py
-def query_test(arg):
+def query_test(index, column, table):
+    return -1
     return "center"
